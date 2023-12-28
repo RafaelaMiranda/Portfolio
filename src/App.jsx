836ad9563545cd0ee3d16900dd5fa5dddd58623c
Rafaela Miranda <dev-rafaelamiranda@proton.me>
2023-12-28 20:03:21 -0300

    ✨ feat:
    Adição de favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from './components/header/Header';
 import './App.css';
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
   return (
     <>
       <Header />
+      <SpeedInsights />
     </>
   );
 }
