7fdc0c19f29856f4c07002d51f675b412c60434a
Rafaela Miranda <dev-rafaelamiranda@proton.me>
2023-12-28 19:35:05 -0300

    🔧 build:
    Remoção do analytics da Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import Header from './components/header/Header';
 import './App.css';
-import * as Analytics from '@vercel/analytics';
 
 function App() {
-  
   return (
     <>
       <Header />
-      <Analytics />
     </>
   );
 }
