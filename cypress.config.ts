45cac010002cc16ee6e11b85a3c39a860b2a9137
Rafaela Miranda <dev-rafaelamiranda@proton.me>
2023-12-30 22:25:09 -0300

    🧪 test:
    Adição do cypress

---
+++ cypress.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  projectId: "gq63fp",
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
