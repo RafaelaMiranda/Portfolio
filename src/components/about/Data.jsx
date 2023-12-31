388afe0d5c8ac53a6d032eb72481924ad9c0f2e9
Rafaela Miranda <dev-rafaelamiranda@proton.me>
2023-12-31 12:57:37 -0300

    🦄 refactor:
    Adição de metas de SEO

--- src/components/about/Data.jsx
+++ src/components/about/Data.jsx
@@ -5,7 +5,7 @@ import { FileText } from "@phosphor-icons/react";
 const Data = () => {
   return (
     <div className="about__content">
-      <img src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className="about__img" />
+      <img src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Imagem de um notebook em cima de uma mesa" className="about__img" width={480} height={320} fetchPriority="low" />
       <div className="about__data">
         <h3 className="about__stack-title">Sobre mim</h3>
         <p className="about__description">
--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -3,15 +3,16 @@ import Social from "./Social";
 import coffee from "../../assets/coffee.png";
 import globe from "../../assets/globe.png";
 
+
 const Data = () => {
   return (
     <div className="home__data">
       <h1 className="home__title">
         Rafaela Miranda
-        <img src={coffee} alt="ícone de uma xícara de café" className="home__coffee" />
+        <ConvertImage image={coffee} alt="ícone de uma xícara de café" className="home__coffee" />
       </h1>
       <h3 className="home__subtitle">Desenvolvedora Web</h3>
-      <p className="home__description">Desenvolvedora web apaixonada e dedicada, em constante busca por inovação e desafios para aprimorar suas habilidades, contribuindo para projetos notáveis no universo digital. <img src={globe} alt="ícone de um globo" width={16} /></p>
+      <p className="home__description">Desenvolvedora web apaixonada e dedicada, em constante busca por inovação e desafios para aprimorar suas habilidades, contribuindo para projetos notáveis no universo digital. <img src={globe} alt="ícone de um globo" width={16} height={16} /></p>
       <Social />
     </div>
   )
