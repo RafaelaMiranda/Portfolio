e9ac4168780c0274a76dea1500ecaf51e3749817
Rafaela Miranda <dev-rafaelamiranda@proton.me>
2023-12-31 13:20:18 -0300

    🦄 refactor:
    Adição de metas de SEO

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Social from "./Social";
-import coffee from "../../assets/coffee.png";
-import globe from "../../assets/globe.png";
+import coffee from "../../assets/coffee.webp";
+import globe from "../../assets/globe.webp";
 
 const Data = () => {
   return (
     <div className="home__data">
       <h1 className="home__title">
         Rafaela Miranda
-        <img src={coffee} alt="ícone de uma xícara de café" className="home__coffee" />
+        <img src={coffee} alt="ícone de uma xícara de café" className="home__coffee" width={60} height={60} />
       </h1>
       <h3 className="home__subtitle">Desenvolvedora Web</h3>
       <p className="home__description">Desenvolvedora web apaixonada e dedicada, em constante busca por inovação e desafios para aprimorar suas habilidades, contribuindo para projetos notáveis no universo digital. <img src={globe} alt="ícone de um globo" width={16} height={16}/></p>
