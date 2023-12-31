203e79aea53e8c2b7584f93485aef18065b32c0d
Rafaela Miranda <dev-rafaelamiranda@proton.me>
2023-12-31 13:12:25 -0300

    🦄 refactor:
    Adição de metas de SEO

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -2,17 +2,16 @@ import React from "react";
 import Social from "./Social";
 import coffee from "../../assets/coffee.png";
 import globe from "../../assets/globe.png";
-import { ConvertImage } from 'react-convert-image';
 
 const Data = () => {
   return (
     <div className="home__data">
       <h1 className="home__title">
         Rafaela Miranda
-        <ConvertImage image={coffee} alt="ícone de uma xícara de café" className="home__coffee" />
+        <img src={coffee} alt="ícone de uma xícara de café" className="home__coffee" />
       </h1>
       <h3 className="home__subtitle">Desenvolvedora Web</h3>
-      <p className="home__description">Desenvolvedora web apaixonada e dedicada, em constante busca por inovação e desafios para aprimorar suas habilidades, contribuindo para projetos notáveis no universo digital. <img src={globe} alt="ícone de um globo" width={16} height={16} /></p>
+      <p className="home__description">Desenvolvedora web apaixonada e dedicada, em constante busca por inovação e desafios para aprimorar suas habilidades, contribuindo para projetos notáveis no universo digital. <img src={globe} alt="ícone de um globo" width={16} height={16}/></p>
       <Social />
     </div>
   )
